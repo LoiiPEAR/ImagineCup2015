f8200fc6107af6c523f50403a9ca66ac98b3927c
Loii-PEAR <c.pprae@gmail.com>
2015-02-25 23:07:46 +0700

    Create a log in page

---
+++ DEDI/DEDI.Shared/LogInPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Navigation;
+
+namespace DEDI
+{
+    public sealed partial class LogInPage
+    {
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+    }
+}
--- DEDI/DEDI.WindowsPhone/LogInPage.xaml.cs
+++ DEDI/DEDI.WindowsPhone/LogInPage.xaml.cs
@@ -20,11 +20,13 @@ namespace DEDI
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class LogInPage : Page
     {
-        public MainPage()
+        public LogInPage()
         {
             this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
         }
     }
 }
