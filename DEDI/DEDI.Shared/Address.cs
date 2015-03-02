6c9dad6b9f0cc6f56b3a20afd696e87fb9d8b50f
bownormal <phanumas.kan@gmail.com>
2015-03-02 17:59:29 +0700

    Map test

---
+++ DEDI/DEDI.Shared/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace DEDI
+{
+    [DataContract]
+    public class Address
+    {
+        [DataMember]
+        public double Lat { get; set; }
+        [DataMember]
+        public double Lon { get; set; }
+        [DataMember]
+        public string LocationName { get; set; }
+    }
+}
--- DEDI/DEDI.Windows/MapsView.xaml.cs
+++ DEDI/DEDI.Windows/MapsView.xaml.cs
@@ -20,16 +20,8 @@ namespace DEDI
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class LogInPage : Page
+    public sealed partial class MapsView : Page
     {
-        public LogInPage()
-        {
-            this.InitializeComponent();
-        }
-
-        private void gotoMap(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(MapsView));
-        }
+        
     }
 }
