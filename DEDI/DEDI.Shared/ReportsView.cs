456a39c3506f51c8486ae1e3c0f3e8664cbb8ea1
Loii-PEAR <c.pprae@gmail.com>
2015-03-19 05:25:00 +0700

    add map view and report view

---
+++ DEDI/DEDI.Shared/ReportsView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Navigation;
+
+namespace DEDI
+{
+    public sealed partial class ReportsView
+    {
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
