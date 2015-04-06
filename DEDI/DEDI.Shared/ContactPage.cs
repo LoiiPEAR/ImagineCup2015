cf8811829fab8efc84b8de61841aa532eb773fec
bownormal <phanumas.kan@gmail.com>
2015-04-06 15:13:56 +0700

    Message

--- DEDI/DEDI.Shared/ContactPage.cs
+++ DEDI/DEDI.Shared/ContactPage.cs
@@ -21,25 +21,7 @@ namespace DEDI
         {
 
             List<Health_Worker> hw = await App.MobileService.GetTable<Health_Worker>().ToListAsync();
-            //List<GroupInfoList<object>> Groups = new List<GroupInfoList<object>>();
-            // GroupInfoList<object> info = new GroupInfoList<object>();
-            //info.Key =hw[0].position;
-            //foreach (Health_Worker h in hw)
-            //{
-            //    if (h.position == info.Key)
-            //    {
-            //        info.Add(h);
-            //    }
-            //    else
-            //    {
-            //        Groups.Add(info);
-            //        info = new GroupInfoList<object>();
-            //        info.Key = h.position;
-            //        info.Add(h);
-            //    }
-            //}
-            //Groups.Add(info);
-
+           
             var h = hw.GroupBy(u => u.position).OrderBy(t => t.Key.ToString()); ;
             DefaultViewModel["Groups"] = h;
         }
