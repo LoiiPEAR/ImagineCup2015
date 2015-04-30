f813184149866f6f99e752dd1d98bc6a45798f0b
bownormal <phanumas.kan@gmail.com>
2015-04-30 13:48:07 +0700

    da

--- DEDI/DEDI.Windows/MapsView.xaml.cs
+++ DEDI/DEDI.Windows/MapsView.xaml.cs
@@ -235,7 +235,7 @@ namespace DEDI
            {
                //string Title = "Chance of cholera:" + disease_reports[0].cholera + "\nChance of shigella:" + disease_reports[0].shigella + "\nChance of salmonella:" + disease_reports[0].simolnelle + "\nChance of rotavirus:" + disease_reports[0].rotavirus + "\nChance of others:" + disease_reports[0].others;
                string Title = "Probability of this disease report";
-               string Content = "Cholera: " + d[0].cholera + "\n" + "Shigella: " + d[0].shigella + "\n" + "Salmoella: " + d[0].salmonella + "\n" + "Rotavirus: " + d[0].rotavirus + "\n" + "Others: " + d[0].others + "\n\n" + d[0].ocurred_time.Date;
+               string Content = "Cholera: " + Math.Round((d[0].cholera * 100), 2) + " %" + "\n" + "Shigella: " + Math.Round((d[0].shigella * 100), 2) + " %" + "\n" + "Salmoella: " + Math.Round((d[0].salmonella * 100), 2) + " %" + "\n" + "Rotavirus: " + Math.Round((d[0].rotavirus * 100), 2) + " %" + "\n" + "Others: " + Math.Round((d[0].others * 100), 2) + " %" + "\n\n" + d[0].ocurred_time.Date;
                dialog = new MessageDialog(Content, Title);
                await dialog.ShowAsync();
 
